0048d7be71b87b1268ce7ab70ed54a0890904b6b
李炎 <18525589998@163.com>
2021-09-28 04:25:20 +0800

    修改Jenkins

--- jenkins-deploy.sh
+++ jenkins-deploy.sh
@@ -22,45 +22,14 @@ b_path=$(pwd)
 # Opt Path
 opt_path="$b_path/build"
 
-# package 签名
-package_lock="${b_path}/package-lock.json"
-package_md5_file="${b_path}/package-md5"
-touch $package_md5_file
-package_md5=$(head -n 1 ${package_md5_file})
-package_md5_changed=0
-
-# 校验 package 版本
-check_version() {
-  echo "==========比较依赖版本=========="
-
-  current_package_md5=$(md5sum ${package_lock} | cut -d" " -f 1)
-
-  echo -e "$cyan prev: ${package_md5} current: ${current_package_md5}  $none"
-
-  if [ "$current_package_md5" != "$package_md5" ]; then
-    echo "==========依赖有变化=========="
-    echo $current_package_md5 >${package_md5_file}
-    package_md5_changed=1
-  else
-    echo "==========依赖无变化=========="
-  fi
-}
-
-
 # 发布
 deploy() {
 	cd $b_path
 
+  echo "==========0. 安装依赖=========="
 
-check_version
-
-  if [[ $package_md5_changed -eq 1 || $install_dependencies -eq 1 ]]; then
-    echo "==========0. 安装依赖=========="
-    pwd
-
-    # 安装依赖
-    npm install --registry=https://registry.npm.taobao.org --unsafe-perm=true --allow-root
-  fi
+  # 安装依赖
+  npm install --registry=https://registry.npm.taobao.org --unsafe-perm=true --allow-root
 
   echo "==========1. 开始构建=========="
   pwd
