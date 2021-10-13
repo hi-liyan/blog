e8fe0cf8c828a3e075a0e724a9b1b00d33f52b13
李炎 <18525589998@163.com>
2021-10-13 11:42:01 +0800

    ShadowBox 带阴影的盒子

---
+++ src/components/ShadowBox/index.js
@@ -0,0 +1,33 @@
+import React, {Component} from "react";
+import './index.scss'
+
+/**
+ * <pre>
+ *   ShadowBox 带有阴影的容器
+ *
+ *   属性：
+ *    img: 图片地址（如果容器只显示图片，可以使用闭合标签）
+ *
+ *   示例：
+ *    <ShadowBox img='https://images.shiguangping.com/imgs/20211013095137.png'/>
+ *    <ShadowBox>Hello World</ShadowBox>
+ * </pre>
+ */
+class ShadowBox extends Component {
+
+  constructor(props, context) {
+    super(props, context);
+  }
+
+  render() {
+    const {children, img} = this.props
+    return (
+      <div className='containerBox'>
+        <div>{img ? <img src={img} alt=""/> : null}</div>
+        {children}
+      </div>
+    );
+  }
+}
+
+export default ShadowBox
