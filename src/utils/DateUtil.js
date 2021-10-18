a22412b5dc1a49aaee31b421295a5a4d631582e6
李炎 <18525589998@163.com>
2021-10-18 14:56:41 +0800

    1

---
+++ src/utils/DateUtil.js
@@ -0,0 +1,24 @@
+/**
+ * 计算指定日期距离今天的天数
+ * @param past 过去日期 示例：2020-08-17
+ * @returns {number} 天数
+ */
+export function calThePastToToday(past) {
+  const s1 = new Date(past.replace(/-/g, "/"));
+  const s2 = new Date(); // 当前日期：2021-10-18
+  const days = s2.getTime() - s1.getTime();
+  const time = parseInt(days / (1000 * 60 * 60 * 24));
+  return time
+}
+
+/**
+ * 计算指定日期距离今天的天数
+ * @param past 过去日期 示例：2020-08-17
+ * @returns {string} 天数 如 1年零20天
+ */
+export function calThePastToToday_1(past) {
+  const time = calThePastToToday(past)
+  const years = Math.floor(time / 365)
+  const days = Math.ceil(time % 365)
+  return (years > 0 ? (days > 0 ? (years + '年零') : (years + '年')) : null) + (days > 0 ? (days + '天') : null)
+}
