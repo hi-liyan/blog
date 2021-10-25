825d62a239758d4d058bada7ce45e15c8c4a9ce2
李炎 <18525589998@163.com>
2021-10-25 20:58:46 +0800

    1

--- src/components/MarkPen/index.js
+++ src/components/MarkPen/index.js
@@ -1,14 +1,15 @@
 import React, {Component} from "react";
+import PropTypes from 'prop-types'
 
 /**
  * <pre>
  *  MarkPen 马克笔
  *
  *  属性：
- *  display：展示方式
- *  color：马克杯颜色
- *  fontWeight：字体粗细
+ *  noMark：去掉背景颜色
  *  underline: 文字下划线（red）
+ *  color：马克笔颜色
+ *  fontWeight：字体粗细
  *
  *  示例：
  *  <MarkPen color='#000' fontWeight='bold'>Hello World</MarkPen>
@@ -21,23 +22,30 @@ class MarkPen extends Component {
   }
 
   render() {
-    const {display, color, fontWeight, underline} = this.props
+    const {noMark, color, fontWeight, underline, children} = this.props
     return (
-      <div style={{
-        display: display ? display : 'inline-block',
+      <span style={{
+        display: 'inline-block',
         borderRadius: '5px',
         padding: '0 3px',
-        backgroundColor: color ? color : (underline ? 'null' : '#ffedc9'),
+        backgroundColor: noMark ? 'null' : (color ? color : '#ffedc9'),
         color: '#000',
         fontWeight: fontWeight
       }}>
         <span style={{
           borderBottom: underline ? '1px solid #f00' : null
-        }}>{this.props.children}</span>
-      </div>
+        }}>{children}</span>
+      </span>
     );
   }
+}
 
+MarkPen.proptypes = {
+  noMark: PropTypes.bool,
+  color: PropTypes.string,
+  fontWeight: PropTypes.string,
+  underline: PropTypes.bool,
+  children: PropTypes.string
 }
 
 export default MarkPen
