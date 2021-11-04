d7c4dc8bcd8b33e8f95ed924d40390d8aeaaf38d
李炎 <18525589998@163.com>
2021-11-04 23:36:03 +0800

    团建

---
+++ src/components/ImgBox/index.jsx
@@ -0,0 +1,35 @@
+import React, {Component} from "react";
+import {Image} from "rebass";
+import PropTypes from 'prop-types'
+
+/**
+ * <pre>
+ *   ImgBox 图片
+ *
+ *   api：
+ *     src: 照片源
+ *     width: 照片宽度，默认值 50%
+ * </pre>
+ */
+export default class ImgBox extends Component {
+  constructor(props, context) {
+    super(props, context);
+  }
+
+  render() {
+    const {src, width='50%'} = this.props
+    return (
+      <div style={{marginBottom: 20}}>
+        <Image src={src} sx={{
+          width: width,
+          borderRadius: 8,
+        }}/>
+      </div>
+    );
+  }
+}
+
+ImgBox.proptypes = {
+  src: PropTypes.string,
+  width: PropTypes.string
+}
