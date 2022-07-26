cd1bd2d79b0c8b2bc509a8354dc75f138eb6d59c
李炎 <18525589998@163.com>
2022-07-26 20:45:25 +0800

    升级部分组件版本

--- src/utils/date-util.ts
+++ src/utils/date-util.ts
@@ -3,12 +3,12 @@
  * @param past 过去日期 示例：2020-08-17
  * @returns {number} 天数
  */
-export const calThePastToToday = (past: string) => {
-  const s1 = new Date(past.replace(/-/g, "/"));
-  const s2 = new Date(); // 当前日期：2021-10-18
-  const days = s2.getTime() - s1.getTime();
-  const time = Number(days / (1000 * 60 * 60 * 24));
-  return time
+export const calThePastToToday = (past: string): number => {
+    const s1 = new Date(past.replace(/-/g, "/"));
+    const s2 = new Date(); // 当前日期：2021-10-18
+    const days = s2.getTime() - s1.getTime();
+    const time = Number(days / (1000 * 60 * 60 * 24));
+    return time
 }
 
 /**
@@ -16,9 +16,9 @@ export const calThePastToToday = (past: string) => {
  * @param past 过去日期 示例：2020-08-17
  * @returns {string} 天数 如 1年零20天
  */
-export const calThePastToTodayOptString = (past: string) => {
-  const time = calThePastToToday(past)
-  const years = Math.floor(time / 365)
-  const days = Math.ceil(time % 365)
-  return (years > 0 ? (days > 0 ? (years + '年零') : (years + '年')) : null) + (days > 0 ? (days + '天') : null)
+export const calThePastToTodayOptString = (past: string): string => {
+    const time = calThePastToToday(past)
+    const years = Math.floor(time / 365)
+    const days = Math.ceil(time % 365)
+    return (years > 0 ? (days > 0 ? (years + '年零') : (years + '年')) : null) + (days > 0 ? (days + '天') : null)
 }
