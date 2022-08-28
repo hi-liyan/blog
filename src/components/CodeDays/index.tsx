067eb3adb7749d3be14c8b44d49d09a680685cdd
李炎 <18525589998@163.com>
2022-08-28 18:56:35 +0800

    修改样式

--- src/components/CodeDays/index.tsx
+++ src/components/CodeDays/index.tsx
@@ -1,16 +1,13 @@
 import {calThePastToTodayOptString} from "@site/src/utils/date-util";
 import React from "react";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 
 /**
  * 从事开发工作至今天数
  */
 export const CodeDays = () => {
-  return (
-      <span style={{
-        color: '#4042E2',
-        fontWeight: 'bolder'
-      }}>
+
+    return (<span style={{color: '#827397', fontWeight: 'bolder'}}>
       {calThePastToTodayOptString("2020-08-17")}
-    </span>
-  )
+    </span>)
 }
