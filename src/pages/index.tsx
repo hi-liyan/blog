3f4e5d442368c6dcb9b2993dad77b54580e07f38
李炎 <18525589998@163.com>
2022-08-28 11:43:06 +0800

    调整样式；升级组件版本；

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,7 +14,7 @@ function HomepageHeader() {
   return (
       <header className={clsx(styles.heroBanner)}>
         <div className="container">
-          <img src={logo} style={{width: 280, borderRadius: '50%'}} />
+          <img className="homepageLogo" src={logo} />
           <h1 className="hero__title">{siteConfig.title}</h1>
           <p className="hero__subtitle">{siteConfig.tagline}</p>
           <div className={styles.buttons}>
